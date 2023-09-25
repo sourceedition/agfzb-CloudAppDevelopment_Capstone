3a90a8198d6c2da20f7b64be1fb864b9b43f5f60
sourceedition <plumball33@gmail.com>
2023-09-25 16:31:48 -0400

    Adding temporary changes to Github

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -11,21 +11,28 @@ REVIEWS_BASE_URL = 'https://plumball33-5000.theiadocker-0-labs-prod-theiak8s-4-t
 # Create a `get_request` to make HTTP GET requests
 # e.g., response = requests.get(url, params=params, headers={'Content-Type': 'application/json'},
 #                                     auth=HTTPBasicAuth('apikey', api_key))
-def get_request(url, api_key=None, **kwargs):
-    print(kwargs)
+def get_request(url, **kwargs):
+    
+    # If argument contain API KEY
+    api_key = kwargs.get("zrFckKw49zQL865fsImEG1GzvU70YyKWnG2ZysApTbbs")
     print("GET from {} ".format(url))
     try:
-        # Create headers with the API key
-        headers = {
-            'Content-Type': 'application/json',
-            'Authorization': 'Basic {}'.format(api_key)
-        }
-        
-        # Call get method of requests library with URL, headers, and parameters
-        response = requests.get(url, headers=headers, params=kwargs)
+        if api_key:
+            params = dict()
+            params["text"] = kwargs["text"]
+            params["version"] = kwargs["version"]
+            params["features"] = kwargs["features"]
+            params["return_analyzed_text"] = kwargs["return_analyzed_text"]
+            response = requests.get(url, params=params, headers={'Content-Type': 'application/json'},
+                                    auth=HTTPBasicAuth('apikey', api_key))
+        else:
+            # Call get method of requests library with URL and parameters
+            response = requests.get(url, headers={'Content-Type': 'application/json'},
+                                    params=kwargs)
     except:
         # If any error occurs
         print("Network exception occurred")
+
     status_code = response.status_code
     print("With status {} ".format(status_code))
     json_data = json.loads(response.text)
@@ -78,29 +85,20 @@ def get_dealers_from_cf(url, **kwargs):
 
     return results
 
-def get_dealer_by_id(dealer_url, dealer_id):
-    # Call get_request with the base URL for dealerships and dealerId parameter
-    url = REVIEWS_BASE_URL.format(dealer_id=dealer_id)
-    json_result = get_request(url)
+def get_dealer_by_id(url, dealer_id):
+    json_result = get_request(url, dealer_id=dealer_id)
+    print('json_result from line 54',json_result)
 
-    results = []
-    if json_result and "docs" in json_result:
-        dealers = json_result["docs"]
-        for dealer in dealers:
-            # Create a CarDealer object with values in `dealer` dictionary
-            dealer_obj = CarDealer(
-                address=dealer.get("address", ""),
-                city=dealer.get("city", ""),
-                full_name=dealer.get("full_name", ""),
-                dealer_id=dealer.get("id", ""),
-                lat=dealer.get("lat", ""),
-                long=dealer.get("long", ""),
-                short_name=dealer.get("short_name", ""),
-                st=dealer.get("st", ""),
-                zip=dealer.get("zip", "")
-            )
-            results.append(dealer_obj)
-    return results
+    if json_result:
+        dealers = json_result
+        
+    
+        dealer_doc = dealers[0]
+        dealer_obj = CarDealer(address=dealer_doc["address"], city=dealer_doc["city"],
+                                dealer_id=dealer_doc["id"], lat=dealer_doc["lat"], long=dealer_doc["long"], full_name=dealer_doc["full_name"],
+                                
+                                st=dealer_doc["st"], zip=dealer_doc["zip"], short_name=dealer_doc["short_name"])
+    return dealer_obj
 
 def get_dealers_by_state(state):
     # Call get_request with the base URL for dealerships and state parameter
